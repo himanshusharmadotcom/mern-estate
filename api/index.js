ed7715e6570a663749ce6eace49e507dc13b6976
Himanshu <hs7772043@gmail.com>
2024-01-04 23:35:11 +0530

    created error handling middleware and made the sign up page functional with UI and made the proxy server

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 dotenv.config(); //initialize the dotenv
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
 }).catch((err) => {
     console.log(err)
@@ -20,4 +20,14 @@ app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     console.log('Server listening at port 3000.')
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
