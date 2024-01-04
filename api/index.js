323aea0276a103d91f5c05607cab7a2907ff0366
Himanshu <hs7772043@gmail.com>
2024-01-04 22:48:09 +0530

    create sign up api and saved data in mongodb

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config(); //initialize the dotenv
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,6 +16,7 @@ const app = express()
 app.use(express.json()); //allows the client to send and reciece data in json format
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     console.log('Server listening at port 3000.')
