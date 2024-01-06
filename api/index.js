6e026812dba64f960216eaad9e7d893939fbf334
Himanshu <hs7772043@gmail.com>
2024-01-06 22:14:51 +0530

    pre build configuration

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config(); //initialize the dotenv
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,19 +14,27 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err)
 })
 
+const __dirname = path.resolve(); // captures the absolute path of the directory where this code is being executed
+
 const app = express()
 
 app.use(express.json()); //allows the client to send and reciece data in json format
-
+ 
 app.use(cookieParser());
+
+app.listen(3000, () => {
+    console.log('Server listening at port 3000.')
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.listen(3000, () => {
-    console.log('Server listening at port 3000.')
-});
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
